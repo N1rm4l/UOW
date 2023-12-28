464db909e201a4c764a83e9d7195d8a48f2a2496
Nirmal K <115354536+N1rm4l@users.noreply.github.com>
2023-12-28 06:43:07 +0530

    Initial Push

---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using EF.GenericRepository.ISvcs;
+using EF.GenericRepository.Svcs;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EF.GenericRepository
+{
+    public static class Startup
+    {
+        public static void GenericRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        }
+    }
+}
